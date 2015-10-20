2477bb8d9e04a43b321262d8353b052013bca4de
André Abadesso <andre.abadesso@gmail.com>
2015-10-20 10:15:35 -0200

    make the quote clickable to get a new random one that is unused and loop quotes when quote array ends

--- js/main.js
+++ js/main.js
@@ -1,26 +1,51 @@
-document.addEventListener("DOMContentLoaded", function() {
-    loadQuotes();
-});
+(function(document) {
+    'use strict';
 
-function loadQuotes() {
-    var request = pegasus('quotes.json');
-    request.then(function(data) {
-        showQuotes(data.quotes);
+    var quotes = [];
+    var usedQuotes = [];
+    var intervalId = null;
+
+    document.addEventListener('DOMContentLoaded', function() {
+        loadQuotes();
+        document.querySelector('#sentence')
+            .addEventListener('click', showQuote);
     });
-}
 
-function showQuotes(quotes) {
-    var SENTENCE_ID = "sentence";
-    var AUTHOR_ID   = "author";
+    function loadQuotes() {
+        var request = pegasus('quotes.json');
+        request.then(function(data) {
+            quotes = data.quotes;
+            showQuote(loadRandomQuote());
+        });
+    }
 
-    var sentenceElement = document.getElementById(SENTENCE_ID);
-    var authorElement   = document.getElementById(AUTHOR_ID);
-    var quote = quotes[getRandomInt(0, quotes.length)];
+    function loadRandomQuote() {
+        if (quotes.length > 0) {
+            var randomIndex = getRandomInt(0, quotes.length);
+            var quote = quotes.splice(randomIndex, 1)[0];
+            usedQuotes.push(quote);
+            return quote;
+        } else {
+            quotes = usedQuotes.slice();
+            usedQuotes.length = 0;
+            var randomIndex = getRandomInt(0, quotes.length);
+            return quotes.splice(randomIndex, 1)[0];
+        }
+    }
 
-    sentenceElement.textContent = quote.sentence + ".";
-    authorElement.textContent = quote.author;
-}
+    function showQuote() {
+        var SENTENCE_ID = 'sentence';
+        var AUTHOR_ID   = 'author';
 
-function getRandomInt(min, max) {
-    return Math.floor(Math.random() * (max - min)) + min;
-}
+        var sentenceElement = document.getElementById(SENTENCE_ID);
+        var authorElement   = document.getElementById(AUTHOR_ID);
+        var quote = loadRandomQuote();
+
+        sentenceElement.textContent = quote.sentence + '.';
+        authorElement.textContent = quote.author;
+    }
+
+    function getRandomInt(min, max) {
+        return Math.floor(Math.random() * (max - min)) + min;
+    }
+}(document));
